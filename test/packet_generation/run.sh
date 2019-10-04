9543fad6271115e288345021b58553cd5a00fdd5
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-10-04 16:47:43 -0400

    Fixed packet gen

--- test/packet_generation/run.sh
+++ test/packet_generation/run.sh
@@ -3,11 +3,11 @@
 #set -x
 set -euo pipefail
 
-NF_NAME=packet_test
+NF_NAME=packet-gen
 M_CORE=0
 
 #PORT_OPTIONS="dpdk:eth_pcap0,rx_pcap=data/http_lemmy.pcap,tx_pcap=/tmp/out.pcap"
 PORT_ONE="0000:01:00.0"
 PORT_TWO="0000:01:00.1"
 
-../../build.sh run $NF_NAME -n " Pkt gen testing" -m $M_CORE -c 2 -c 3 -c 4 -c 5 -p $PORT_ONE -p $PORT_TWO
+../../build.sh run $NF_NAME -n " Pkt gen testing" -m $M_CORE -c 2 -p $PORT_ONE -p $PORT_TWO
