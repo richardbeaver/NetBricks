dcf594045daa40a3aa9475b746dd09c8b8fa37e0
jethrosun <jethro.s.sun@gmail.com>
2020-01-02 17:42:06 -0500

    expr checkpoint

--- test/maglev/src/main.rs
+++ test/maglev/src/main.rs
@@ -66,8 +66,8 @@ fn main() {
 
             let mut pkts_so_far = (0, 0);
             let mut last_printed = 0.;
-            const MAX_PRINT_INTERVAL: f64 = 120.;
-            const PRINT_DELAY: f64 = 60.;
+            const MAX_PRINT_INTERVAL: f64 = 30.;
+            const PRINT_DELAY: f64 = 15.;
             let sleep_delay = (PRINT_DELAY / 2.) as u64;
             let mut start = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
             let sleep_time = Duration::from_millis(sleep_delay);
