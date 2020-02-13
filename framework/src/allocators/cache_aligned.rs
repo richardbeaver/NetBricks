46e18275933b64902a0044acc304f4d1bc5d5fcd
jethrosun <jethro.s.sun@gmail.com>
2020-02-13 15:19:09 -0500

    Fixed the change in alloc

--- framework/src/allocators/cache_aligned.rs
+++ framework/src/allocators/cache_aligned.rs
@@ -2,7 +2,9 @@
 ///
 /// Using _Unique types_ in the implementation of data structure for packets thus we don't need to
 /// worry about packet isolation.
-use std::alloc::{self, Alloc, Layout};
+///
+use std::alloc::AllocRef;
+use std::alloc::{self, alloc, Layout};
 use std::fmt;
 use std::mem::size_of;
 use std::ops::{Deref, DerefMut};
