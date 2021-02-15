e07ab5724172c8b792cfefa65624107812e6d753
Shuwen Sun <sun.shuw@northeastern.edu>
2021-02-14 20:48:06 -0500

    It's possible! you can turn a 50-line code chunk into just 3 lines. Here's how

--- pvnf/p2p/src/utils.rs
+++ pvnf/p2p/src/utils.rs
@@ -96,6 +96,9 @@ pub fn bt_run_torrents_bak(workload: &str, setup: usize) -> Result<()> {
 /// Run BitTorrent jobs via deluge console
 pub fn bt_run_torrents(workload: Vec<i64>) -> Result<()> {
     let mut argv = Vec::new();
+    argv.push("sudo".to_string());
+    argv.push("-u".to_string());
+    argv.push("jethros".to_string());
     argv.push("/home/jethros/dev/pvn/utils/p2p_expr/p2p_run_nb.sh".to_string());
     argv.push(workload.len().to_string());
     println!("workload {:?}", workload);
