e0f9cfb405260efd7a8cc4da4454594c86b8d537
Shuwen Sun <sun.shuw@northeastern.edu>
2021-10-20 12:32:55 -0400

    :q!

--- pvnf/tlsv/src/utils.rs
+++ pvnf/tlsv/src/utils.rs
@@ -149,7 +149,7 @@ pub fn unordered_validate(
             if let Ok(chain) = certs {
                 let result = try_extracted_cert(chain, dns_name);
                 *cert_count += 1;
-                if *cert_count % 100_000_usize == 0 {
+                if *cert_count % 10_000_usize == 0 {
                     println!("cert count is {} k", *cert_count / 1_000);
                 }
                 if !result {
