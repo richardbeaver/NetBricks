041ae024912e365dad0718351aff6f57dabd10ed
Shuwen Jethro Sun <jethro.sun7@gmail.com>
2022-02-17 15:24:56 -0500

    more ignored words

--- examples.sh
+++ examples.sh
@@ -8,26 +8,26 @@ export examples=(
 	# test/maglev
 	test/chain-test
 	# PVN application
-	test/app-tlsv_t
-	test/app-rdr_t
-	test/app-p2p_t
-	test/app-xcdr_t
+	# test/app-tlsv_t
+	# test/app-rdr_t
+	# test/app-p2p_t
+	# test/app-xcdr_t
 	# PVN NF libraries
-	pvnf/tlsv
-	pvnf/rdr
-	pvnf/p2p
-	pvnf/xcdr
+	# pvnf/tlsv
+	# pvnf/rdr
+	# pvnf/p2p
+	# pvnf/xcdr
 	# PVN chain
-	test/co-tlsv-rdr
-	test/co-rdr-p2p
-	test/co-rdr-xcdr
-	test/co-tlsv-p2p
-	test/co-tlsv-xcdr
-	test/co-xcdr-p2p
+	# test/co-tlsv-rdr
+	# test/co-rdr-p2p
+	# test/co-rdr-xcdr
+	# test/co-tlsv-p2p
+	# test/co-tlsv-xcdr
+	# test/co-xcdr-p2p
 	# New coresident NFs
-	test/co-rdr-xcdr-p2p
-	test/co-tlsv-rdr-p2p
-	test/co-tlsv-p2p-xcdr
-	test/co-tlsv-rdr-xcdr
-	test/co-tlsv-rdr-p2p-xcdr
+	# test/co-rdr-xcdr-p2p
+	# test/co-tlsv-rdr-p2p
+	# test/co-tlsv-p2p-xcdr
+	# test/co-tlsv-rdr-xcdr
+	# test/co-tlsv-rdr-p2p-xcdr
 )
